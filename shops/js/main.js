8410d3557c1ac6ae9457830d4dad533205977bd4
Laurent Hulstaert <laurenthulstaert@gmail.com>
2018-01-26 10:13:39 +0100

    creation the functions to copy object and to sort object

--- shops/js/main.js
+++ shops/js/main.js
@@ -6,7 +6,36 @@ let dataRequest = new XMLHttpRequest();
 let whenDataLoaded = function() { // callback function
   let dataText = dataRequest.responseText; // we store the text of the response
   let dataObject = JSON.parse(dataText); // we convert the text into an object
+  //let dataObjectCopy = copyDataObject(dataObject); // we copy the object before sorting it
+  sortObjectbySpecificKey(dataObject,'Year');
   console.log(dataObject);
+  //console.log(dataObjectCopy);
+}
+
+function sortObjectbySpecificKey(data,key) {
+  // data: object to sort
+  // key: the key on with which the sort will be done
+  data.sort( function (a, b) { // we compare the value
+    if (typeof a[key] === 'string') {
+      return (a[key].toLowerCase() < b[key].toLowerCase()) ? -1 : (a[key].toLowerCase() > b[key].toLowerCase()) ? 1 : 0;
+    } else {
+      return a[key] - b[key];
+    }
+    /*
+      Transcription of the line: return (a[key].toLowerCase() < b[key].toLowerCase()) ? -1 : (a[key].toLowerCase() > b[key].toLowerCase()) ? 1 : 0;
+      if (a[key].toLowerCase()x < y) {
+        return -1;
+      } else if (a[key].toLowerCase() > y) {
+        return 1;
+      } else {
+        return 0;
+      }
+    */
+  });
+}
+
+function copyDataObject(dataObject) { // function to copy an object without any reference
+  return dataObject.slice(0);
 }
 
 
