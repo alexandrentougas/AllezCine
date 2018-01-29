eee442a144855b41643449061096058b48e76aef
Alexandre Ntougas <alexandrentougas@gmail.com>
2018-01-29 14:55:11 +0100

    age check modal done

--- shops/js/main.js
+++ shops/js/main.js
@@ -1,3 +1,31 @@
+$(window).on('load', function() { // age check modal on page load
+  $('#ageWarning').modal('show');
+});
+
+let isItOlderThan18 = function(year, month, day) { // checks if user is older than 18 years old and returns true/false
+  return new Date(year + 18, month - 1, day) <= new Date();
+};
+
+$('#ageVerif').click(function() { // 
+  if (isItOlderThan18(Number($('#birthInput').val().split('-')[0]), Number($('#birthInput').val().split('-')[1]), Number($('#birthInput').val().split('-')[2])) === true) {
+    $('#ageWarning').modal('hide');
+  } else {
+    location.href='http://www.imdb.com/?ref_=nv_home';
+  };
+});
+
+$('#loginModal').on('shown.bs.modal', function() {
+  $('#username').trigger('focus')
+});
+
+$('#registerModal').on('shown.bs.modal', function() {
+  $('#name').trigger('focus')
+});
+
+$('#newAccount').click(function() {
+  $('#registerModal').modal('show');
+});
+
 let moviesFeed = 'https://laurenthu.github.io/AllezCine/shops/database/movies.json';
 let tvShowsFeed = 'https://laurenthu.github.io/AllezCine/shops/database/tvshows.json';
 
@@ -7,12 +35,12 @@ let dataRequest2 = new XMLHttpRequest();
 let whenDataLoadedMovies = function() { // callback function
   let dataText = dataRequest.responseText; // we store the text of the response
   let dataObject = JSON.parse(dataText); // we convert the text into an object
-  sortObjectbySpecificKey(dataObject,'Year','DESC');
+  sortObjectbySpecificKey(dataObject, 'Year', 'DESC');
   console.log(dataObject);
   //console.log(dataWithoutFalseValueOnSpecificKey(dataObject,'Slider'));
   //console.log(xLastElementsAccordingSpecificKey(dataObject,'Year',6));
-  for(let i = 0; i < 6; i++) {
-    createHTMLMovieItem(dataObject[i],'#top-movie .movie-list .row','top-movie');
+  for (let i = 0; i < 6; i++) {
+    createHTMLMovieItem(dataObject[i], '#top-movie .movie-list .row', 'top-movie');
   }
 }
 
@@ -20,15 +48,15 @@ let whenDataLoadedTvShows = function() {
   let dataText = dataRequest2.responseText;
   let dataObject = JSON.parse(dataText);
   console.log(dataObject);
-  for(let i = 0; i < 6; i++) {
-    createHTMLTvShowItem(dataObject[i], '#featured-tvshows .tvshow-list .row','featured-tvshow');
+  for (let i = 0; i < 6; i++) {
+    createHTMLTvShowItem(dataObject[i], '#featured-tvshows .tvshow-list .row', 'featured-tvshow');
   };
 };
 
-function sortObjectbySpecificKey(data,key,order = 'ASC') {
+function sortObjectbySpecificKey(data, key, order = 'ASC') {
   // data: object to sort
   // key: the key on with which the sort will be done
-  data.sort( function (a, b) { // we compare the value
+  data.sort(function(a, b) { // we compare the value
     if (order === 'ASC') {
       if (typeof a[key] === 'string') {
         return (a[key].toLowerCase() < b[key].toLowerCase()) ? -1 : (a[key].toLowerCase() > b[key].toLowerCase()) ? 1 : 0;
@@ -55,7 +83,7 @@ function sortObjectbySpecificKey(data,key,order = 'ASC') {
   });
 }
 
-function dataWithoutFalseValueOnSpecificKey(data,keyToTest) {
+function dataWithoutFalseValueOnSpecificKey(data, keyToTest) {
   let arrayData = []; // we initialize the array
   for (let key in data) { // we loop our object
     if (data[key][keyToTest] != false) { // if the value is not false
@@ -65,22 +93,22 @@ function dataWithoutFalseValueOnSpecificKey(data,keyToTest) {
   return arrayData; // we return an array of object
 }
 
-function xFirstElementsAccordingSpecificKey(data,key,numberElement) {
+function xFirstElementsAccordingSpecificKey(data, key, numberElement) {
   // return an array with X first elements of the object
   // sort according a specific key
   let arrayData = []; // we initialize the array
-  sortObjectbySpecificKey(data,key,'ASC'); // we sort the object
+  sortObjectbySpecificKey(data, key, 'ASC'); // we sort the object
   for (let i = 0; i < numberElement && i < data.length; i++) { // we loop
     arrayData.push(data[i]); // we insert the value in the array
   }
   return arrayData; // we return the array
 }
 
-function xLastElementsAccordingSpecificKey(data,key,numberElement) {
+function xLastElementsAccordingSpecificKey(data, key, numberElement) {
   // return an array with X last elements of the object
   // sort according a specific key
   let arrayData = []; // we initialize the array
-  sortObjectbySpecificKey(data,key,'DESC'); // we sort the object
+  sortObjectbySpecificKey(data, key, 'DESC'); // we sort the object
   for (let i = 0; i < numberElement && i < data.length; i++) { // we loop
     arrayData.push(data[i]); // we insert the value in the array
   }
@@ -91,58 +119,58 @@ function copyObject(data) { // function to copy an object without any reference
   return data.slice(0); // we return the copy
 }
 
-function createHTMLMovieItem(data,parent,idPrefix) {
+function createHTMLMovieItem(data, parent, idPrefix) {
   let HTMLcontent = '<div class="col-6 col-md-2 card movie-item" id="' + idPrefix + '-' + data['ID'] + '"></div>'; // we open the div, insert class and ID
-  $( HTMLcontent ).appendTo( $( parent ) ); // we add our HTML content to the parent
-  $( '#' + idPrefix + '-' + data['ID'] ).attr({
-                                      'data-id': data['ID'],
-                                      'data-year': data['Year'],
-                                      'data-duration': data['Duration'],
-                                      'data-genre': data['Genre'].join(', ').toLowerCase(),
-                                      'data-director': data['Director'].join(', ').toLowerCase(),
-                                      'data-writers': data['Writers'].join(', ').toLowerCase(),
-                                      'data-actors': data['Actors'].join(', ').toLowerCase(),
-                                      'data-country': data['Country'].toLowerCase(),
-                                    });
-  $( '<img src="img/' + data['Poster'] + '" class="poster card-img-top img-fluid" title="' + data['Title'] + ' (' + data['Year'] + ')" >' ).appendTo( $( '#' + idPrefix + '-' + data['ID']) );
-  $( '<div class="card-body"></div>' ).appendTo( $( '#' + idPrefix + '-' + data['ID']) );
-  $( '<h5 class="card-title">' + data['Title'] + '</h5>' ).appendTo( $( '#' + idPrefix + '-' + data['ID'] + ' .card-body') );
-  $( '<h6 class="card-subtitle">' + data['Year'] + '</h6>' ).appendTo( $( '#' + idPrefix + '-' + data['ID'] + ' .card-body') );
-  $( '<div class="card-text">' + data['Genre'][0] + '</div>' ).appendTo( $( '#' + idPrefix + '-' + data['ID'] + ' .card-body') );
-  $( '<div class="card-footer"></div>' ).appendTo( $( '#' + idPrefix + '-' + data['ID'] + ' .card-body') );
-  $( '<div class="btn-group btn-group-sm" role="group" aria-label="More function"></div>' ).appendTo( $( '#' + idPrefix + '-' + data['ID'] + ' .card-footer') );
-  $( '<button type="button" class="btn btn-secondary"><i class="fa fa-info"></i></button>').appendTo( $( '#' + idPrefix + '-' + data['ID'] + ' .btn-group') );
-  $( '<button type="button" class="btn btn-secondary trailer-lightbox" data-trailer="' + data['Trailer'] + '"><i class="fa fa-youtube-play"></i></button>').appendTo( $( '#' + idPrefix + '-' + data['ID'] + ' .btn-group') );
+  $(HTMLcontent).appendTo($(parent)); // we add our HTML content to the parent
+  $('#' + idPrefix + '-' + data['ID']).attr({
+    'data-id': data['ID'],
+    'data-year': data['Year'],
+    'data-duration': data['Duration'],
+    'data-genre': data['Genre'].join(', ').toLowerCase(),
+    'data-director': data['Director'].join(', ').toLowerCase(),
+    'data-writers': data['Writers'].join(', ').toLowerCase(),
+    'data-actors': data['Actors'].join(', ').toLowerCase(),
+    'data-country': data['Country'].toLowerCase(),
+  });
+  $('<img src="img/' + data['Poster'] + '" class="poster card-img-top img-fluid" title="' + data['Title'] + ' (' + data['Year'] + ')" >').appendTo($('#' + idPrefix + '-' + data['ID']));
+  $('<div class="card-body"></div>').appendTo($('#' + idPrefix + '-' + data['ID']));
+  $('<h5 class="card-title">' + data['Title'] + '</h5>').appendTo($('#' + idPrefix + '-' + data['ID'] + ' .card-body'));
+  $('<h6 class="card-subtitle">' + data['Year'] + '</h6>').appendTo($('#' + idPrefix + '-' + data['ID'] + ' .card-body'));
+  $('<div class="card-text">' + data['Genre'][0] + '</div>').appendTo($('#' + idPrefix + '-' + data['ID'] + ' .card-body'));
+  $('<div class="card-footer"></div>').appendTo($('#' + idPrefix + '-' + data['ID'] + ' .card-body'));
+  $('<div class="btn-group btn-group-sm" role="group" aria-label="More function"></div>').appendTo($('#' + idPrefix + '-' + data['ID'] + ' .card-footer'));
+  $('<button type="button" class="btn btn-secondary"><i class="fa fa-info"></i></button>').appendTo($('#' + idPrefix + '-' + data['ID'] + ' .btn-group'));
+  $('<button type="button" class="btn btn-secondary trailer-lightbox" data-trailer="' + data['Trailer'] + '"><i class="fa fa-youtube-play"></i></button>').appendTo($('#' + idPrefix + '-' + data['ID'] + ' .btn-group'));
 
 
 }
 
-function createHTMLTvShowItem(data,parent,idPrefix) {
+function createHTMLTvShowItem(data, parent, idPrefix) {
   let HTMLcontent = '<div class="col-6 col-md-2 card tvshow-item" id="' + idPrefix + '-' + data['ID'] + '"></div>';
-  $( HTMLcontent ).appendTo( $( parent ) ); // we add our HTML content to the parent
+  $(HTMLcontent).appendTo($(parent)); // we add our HTML content to the parent
   if (data['Ending'] === false) {
     data['Ending'] = "Ongoing";
   };
-  $( '#' + idPrefix + '-' + data['ID'] ).attr({
-                                      'data-id': data['ID'],
-                                      'data-begin': data['Beginning'],
-                                      'data-end': data['Ending'],
-                                      'data-seasons': data['Seasons'],
-                                      'data-episodes': data['Episodes'],
-                                      'data-duration': data['Duration'],
-                                      'data-genre': data['Genre'].join(', ').toLowerCase(),
-                                      'data-creators': data['Creators'].join(', ').toLowerCase(),
-                                      'data-actors': data['Actors'].join(', ').toLowerCase(),
-                                    });
-  $( '<img src="' + data['Poster'] + '" class="poster card-img-top" title="' + data['Title'] + '(' + data['Beginning'] + '-' + data['Ending'] + ')" >' ).appendTo( $( '#' + idPrefix + '-' + data['ID']) );
-  $( '<div class="card-body"></div>' ).appendTo( $( '#' + idPrefix + '-' + data['ID']) );
-  $( '<h5 class="card-title">' + data['Title'] + '</h5>' ).appendTo( $( '#' + idPrefix + '-' + data['ID'] + ' .card-body') );
-  $( '<h6 class="card-subtitle">' + data['Beginning'] + '-' + data['Ending'] + '</h6>' ).appendTo( $( '#' + idPrefix + '-' + data['ID'] + ' .card-body') );
-  $( '<div class="card-text">' + data['Genre'].join(', ') + '</div>' ).appendTo( $( '#' + idPrefix + '-' + data['ID'] + ' .card-body') );
-  $( '<div class="card-footer"></div>' ).appendTo( $( '#' + idPrefix + '-' + data['ID'] + ' .card-body') );
-  $( '<div class="btn-group btn-group-sm" role="group" aria-label="More function"></div>' ).appendTo( $( '#' + idPrefix + '-' + data['ID'] + ' .card-footer') );
-  $( '<button type="button" class="btn btn-secondary"><i class="fa fa-info"></i></button>').appendTo( $( '#' + idPrefix + '-' + data['ID'] + ' .btn-group') );
-  $( '<button type="button" class="btn btn-secondary"><i class="fa fa-youtube-play"></i></button>').appendTo( $( '#' + idPrefix + '-' + data['ID'] + ' .btn-group') );
+  $('#' + idPrefix + '-' + data['ID']).attr({
+    'data-id': data['ID'],
+    'data-begin': data['Beginning'],
+    'data-end': data['Ending'],
+    'data-seasons': data['Seasons'],
+    'data-episodes': data['Episodes'],
+    'data-duration': data['Duration'],
+    'data-genre': data['Genre'].join(', ').toLowerCase(),
+    'data-creators': data['Creators'].join(', ').toLowerCase(),
+    'data-actors': data['Actors'].join(', ').toLowerCase(),
+  });
+  $('<img src="' + data['Poster'] + '" class="poster card-img-top" title="' + data['Title'] + '(' + data['Beginning'] + '-' + data['Ending'] + ')" >').appendTo($('#' + idPrefix + '-' + data['ID']));
+  $('<div class="card-body"></div>').appendTo($('#' + idPrefix + '-' + data['ID']));
+  $('<h5 class="card-title">' + data['Title'] + '</h5>').appendTo($('#' + idPrefix + '-' + data['ID'] + ' .card-body'));
+  $('<h6 class="card-subtitle">' + data['Beginning'] + '-' + data['Ending'] + '</h6>').appendTo($('#' + idPrefix + '-' + data['ID'] + ' .card-body'));
+  $('<div class="card-text">' + data['Genre'].join(', ') + '</div>').appendTo($('#' + idPrefix + '-' + data['ID'] + ' .card-body'));
+  $('<div class="card-footer"></div>').appendTo($('#' + idPrefix + '-' + data['ID'] + ' .card-body'));
+  $('<div class="btn-group btn-group-sm" role="group" aria-label="More function"></div>').appendTo($('#' + idPrefix + '-' + data['ID'] + ' .card-footer'));
+  $('<button type="button" class="btn btn-secondary"><i class="fa fa-info"></i></button>').appendTo($('#' + idPrefix + '-' + data['ID'] + ' .btn-group'));
+  $('<button type="button" class="btn btn-secondary"><i class="fa fa-youtube-play"></i></button>').appendTo($('#' + idPrefix + '-' + data['ID'] + ' .btn-group'));
 }
 
 // We load the data
