1fd301a92e67f30252dadc31f9487d67151e3e4e
laurenthu <laurenthulstaert@gmail.com>
2018-02-05 11:00:30 +0100

    update createSliderItem()

--- shops/js/main.js
+++ shops/js/main.js
@@ -327,9 +327,9 @@ function copyObject(data) { // function to copy an object without any reference
 
 function createSliderItem(data,parent,idPrefix) {
     let HTMLId = idPrefix + '-' + data['ID'];
-    let HTMLContent = '<div id="' + HTMLId + '" class="carousel-item"><div class="container"><div class="carousel-caption"><h1>Latest Online Movies</h1><p>In space no one can hear you scream</p><p><a class="btn btn-lg btn-primary" href="#" role="button">Watch Trailer</a></p></div></div></div>'
+    let HTMLContent = '<div id="' + HTMLId + '" class="carousel-item"><div class="container"><div class="carousel-caption"><h1>Latest <span class="color-primary">On</span>line <span class="color-primary">Mo</span>vies</h1><p>In space no one can hear you scream</p><btn class="btn btn-lg btn-primary" href="#" role="button">Watch Trailer</btn></div></div></div>'
     $(HTMLContent).appendTo($(parent));
-    $('#' + HTMLId).css('background-image', 'linear-gradient(to bottom, rgba(0,0,0,0.5) 0%,rgba(0,0,0,0.5) 100%), url(img/movies/' + data['Slider'] + ')')
+    $('#' + HTMLId).css('background-image', 'linear-gradient(to bottom, rgba(0,0,0,.5) 0%,rgba(0,0,0,.5) 100%), url(img/movies/' + data['Slider'] + ')')
 };
 
 function createHTMLMovieItem(data,parent,idPrefix) { // we create the item for one movie
